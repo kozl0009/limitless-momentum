4416cace7825bd485667eadb3b42b52ca7b51d35
kozl0009 <kozl0009@algonquinlive.com>
2014-03-12 15:00:15 -0400

    corrected main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 
-	$(window).resize(function(){
+	//$(window).resize(function(){
 
 		if ($(window).width() <= 640) {  
 			$('#sidebar').insertBefore('#content');
 		}else{
 			$('#content').insertBefore('#sidebar'); 
 		}
-	});
+	//});
 
 	$(".box").click(function(e){
 	   $(this).find('.meeting-details').slideDown('1000');
